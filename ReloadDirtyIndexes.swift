7fd1aca0ecb9095f2b36246b675c2e4b034ca155
Simon <markhams@tcd.ie>
2018-01-25 10:05:38 +0000

    added reload dirty rows

---
+++ ReloadDirtyIndexes.swift
@@ -0,0 +1,24 @@
+if let dirtySelected = cache.getDirty(), !dirtySelected.isEmpty {
+    reload()
+}
+
+func reloadRows(for items : [Item]) {
+    var indexes = Set<IndexPath>()
+    
+    for item in items {
+        guard let visibeIndexes = tableView?.indexPathsForVisibleRows else {
+            return
+        }
+        
+        if let indexToReload = tableItems.index(of: item) {
+            let newIndexPath = IndexPath(row: indexToReload, section: 0)
+            
+            if visibeIndexes.contains(where: {$0.section == 0 && $0.row == indexToReload }) {
+                indexes.insert(newIndexPath)
+            }
+        }
+    }
+    if indexes.count > 0 {
+        tableView.reloadRows(at: Array(indexes) , with: .none)
+    }
+}
